4e088f7cf36c9d11c76852cd894f4de9ee44017e
Yifan Zuo <yifan051109@gmail.com>
2026-08-27 18:14:43 +0800

    Update Lab 1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,47 @@
-d3.csv("data/students.csv", d => {
+async function loadData() {
 
-    return {
-        name: d.name,
-        score: +d.score
-    };
+    const data = await d3.csv(
+        "../data/students.csv",
+        d => ({
+            name: d.name,
+            score: +d.score
+        })
+    );
 
-}).then(data => {
+    const width = 800;
+    const height = 500;
 
-    console.log(data);
+    const svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-});
+    svg.selectAll("rect")
+        .data(data)
+        .join("rect")
+        .attr("x", (d, i) => 50 + i * 90)
+        .attr("y", d => 400 - d.score * 3)
+        .attr("width", 60)
+        .attr("height", d => d.score * 3)
+        .attr("class", "bar");
 
+    svg.selectAll(".score-label")
+        .data(data)
+        .join("text")
+        .attr("class", "score-label")
+        .attr("x", (d, i) => 80 + i * 90)
+        .attr("y", 425)
+        .attr("text-anchor", "middle")
+        .text(d => d.score);
 
-d3.json("data/students.json")
-    .then(data => {
+    svg.selectAll(".name-label")
+        .data(data)
+        .join("text")
+        .attr("class", "name-label")
+        .attr("x", (d, i) => 80 + i * 90)
+        .attr("y", 450)
+        .attr("text-anchor", "middle")
+        .text(d => d.name);
+}
 
-        console.log(data);
-
-    });
+loadData();
